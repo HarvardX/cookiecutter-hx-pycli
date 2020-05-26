c89dcf5dec0f38298a179e4ce65eaa3861e469e1
nmaekawa <nmaekawa@g.harvard.edu>
2020-05-26 19:15:09 -0400

    initial commit

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for hxann."""
+import contextlib
+import sys
+import click
+
+from dotenv import load_dotenv
+import os
+
+
+# if dotenv file, load it
+dotenv_path = os.environ.get(
+        '{{cookiecutter.project_prefix}}_DOTENV_PATH',
+        None)
+if dotenv_path:
+    # BEWARE that dotenv overrides what's already in env
+    load_dotenv(dotenv_path, override=True)
+
+
+@click.command()
+@click.option(
+    '--arg',
+    default='value'
+    help='some argument for this command',)
+def cli(arg):
+    click.echo('arg is {}'.format(arg))
+
+
+# from http://stackoverflow.com/a/29824059
+@contextlib.contextmanager
+def _smart_open(filename, mode='Ur'):
+    if filename == '-':
+        if mode is None or mode == '' or 'r' in mode:
+            fhandle = sys.stdin
+        else:
+            fhandle = sys.stdout
+    else:
+        fhandle = open(filename, mode)
+
+    try:
+        yield fhandle
+    finally:
+        if filename != '-':
+            fhandle.close()
+
+
+if __name__ == "__main__":
+    sys.exit(cli())  # pragma: no cover
